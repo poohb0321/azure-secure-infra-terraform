d274ac10990e4995f3ee65f15a5c58e7c2f7c10f
Pooja B <pbandla0321@gmail.com>
2025-03-11 02:25:01 -0400

    Initial commit with infrastructure setup

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.22.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
